eb2b6e162c971044adb19be64543113776c98a98
ASUS <shanhtet2149@gmail.com>
2019-07-23 11:21:09 +0630

    first commit.

--- src/main/java/demo/example/blogspring/BlogSpringApplication.java
+++ src/main/java/demo/example/blogspring/BlogSpringApplication.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 
 import java.time.LocalDate;
+import java.util.prefs.Preferences;
 
 
 @SpringBootApplication
@@ -24,6 +25,8 @@ public class BlogSpringApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(BlogSpringApplication.class, args);
+
+        System.out.println("Wwelcome Mesage");
     }
 
     @Bean
