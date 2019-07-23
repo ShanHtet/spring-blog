8f4d86624b303b257125165b5fcc05436eb12e89
ASUS <shanhtet2149@gmail.com>
2019-07-23 11:55:29 +0630

    first commit.

---
+++ src/main/java/demo/example/blogspring/service/AuthorServiceImpl.java
@@ -0,0 +1,22 @@
+package demo.example.blogspring.service;
+
+import demo.example.blogspring.model.Author;
+
+import java.util.List;
+
+public class AuthorServiceImpl implements AuthorService {
+    @Override
+    public List<Author> findAll() {
+        return null;
+    }
+
+    @Override
+    public Author create(Author author) {
+        return null;
+    }
+
+    @Override
+    public Author findById(Long id) {
+        return null;
+    }
+}
